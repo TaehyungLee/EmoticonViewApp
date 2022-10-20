3f29d187691f90c6b602f31d676627ccdd00f3dd
LeeTaehyung <thlee@kb-sys.com>
2022-10-20 11:18:42 +0900

    Initial Commit

---
+++ EmoticonViewApp/EmoticonViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoticonViewAppApp.swift
+//  EmoticonViewApp
+//
+//  Created by Taehyung Lee on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct EmoticonViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
